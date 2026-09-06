1abe48a45d3891938fa1c4aaa663acb112b7b282
pmeorsd-afk <pmeorsd@users.noreply.github.com>
2026-09-06 23:44:07 +0300

    build: Bump app version to 1.6 (versionCode 16)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.music.spotui"
         minSdk = 26
         targetSdk = 34
-        versionCode = 15
-        versionName = "1.5"
+        versionCode = 16
+        versionName = "1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
